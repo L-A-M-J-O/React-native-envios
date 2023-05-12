a221c1e95e0e860f70df1266207cdfaee1fe8791
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-05-12 23:05:46 +0200

    Comenzando a crear App

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 const CatApp = () => {
   return (
     <View>
-      <Image
-        source={{
-          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
-        }}
-        style={{width: 200, height: 200}}
-      />
-      <Text>Hello, I am your cat!</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 };
